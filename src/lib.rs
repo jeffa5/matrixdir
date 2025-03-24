e9dfbdd55ad53b7ad041dcb7fe51f50915a5a6c5
Andrew Jeffery <dev@jeffas.net>
2025-03-24 13:21:17 +1030

    Export some matrix structs directly

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,7 @@ pub mod matrixdir;
 pub mod matrixfile;
 pub mod matrixroomdir;
 pub mod read_write;
+
+pub use matrixdir::MatrixDir;
+pub use matrixroomdir::MatrixRoomDir;
+pub use matrixfile::MatrixFile;
